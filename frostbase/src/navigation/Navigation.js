0a2b3921596e474ca1a1f229b5905905754159a7
SKSKronosUTT <0323105967@ut-tijuana.edu.mx>
2025-06-25 21:38:18 -0700

    Set all tabOptions and their icons

--- frostbase/src/navigation/Navigation.js
+++ frostbase/src/navigation/Navigation.js
@@ -1,6 +1,9 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
+//Icons
+import Feather from '@expo/vector-icons/Feather';
+
 //Screens
 import HomeScreen from "../screens/HomeScreen";
 
@@ -11,14 +14,51 @@ function MyTabs() {
         <Tab.Navigator
             initialRouteName="Home"
             screenOptions={{
-                tabBarActiveTintColor: 'blue'
+                tabBarActiveTintColor: 'blue',
+                tabBarInactiveTintColor: 'gray'
             }}
         >
             <Tab.Screen name="Home" component={HomeScreen} 
                 options={{
                     //headerShown: false
+                    tabBarIcon: (color) => {
+                        return <Feather name="home" color={color} size={20} />
+                    }
                 }}
             />
+            <Tab.Screen name="Map" component={HomeScreen} 
+                options={{
+                    //headerShown: false
+                    tabBarIcon: (color) => {
+                        return <Feather name="map" color={color} size={20} />
+                    }
+                }}
+            />
+            <Tab.Screen name="Analytics" component={HomeScreen} 
+                options={{
+                    //headerShown: false
+                    tabBarIcon: (color) => {
+                        return <Feather name="bar-chart-2" color={color} size={20} />
+                    }
+                }}
+            />
+            <Tab.Screen name="History" component={HomeScreen} 
+                options={{
+                    //headerShown: false
+                    tabBarIcon: (color) => {
+                        return <Feather name="clock" color={color} size={20} />
+                    }
+                }}
+            />
+            <Tab.Screen name="Profile" component={HomeScreen} 
+                options={{
+                    //headerShown: false
+                    tabBarIcon: (color) => {
+                        return <Feather name="user" color={color} size={20} />
+                    }
+                }}
+            />
+
         </Tab.Navigator>
     );
 }
